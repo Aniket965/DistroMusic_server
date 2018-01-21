b3fcaed134091dfd6a2aa9c719890c7426c71784
aniket965 <aniket965.as@gmail.com>
2018-01-21 17:55:30 +0530

    added Sketch

--- client/public/js/sketch.js
+++ client/public/js/sketch.js
@@ -1,6 +1,21 @@
+let song;
+let amp;
 function setup() {
+
+  amp = new p5.Amplitude()
+  song = loadSound('http://localhost:3000/assets/lol.mp3', () => {
+    document.getElementById('playButton').addEventListener('click', () => togglePlaying())
+    console.log('loaded')
+  })
 
 }
 function draw() {
-  
+
+}
+const togglePlaying = () => {
+  if (song.isPlaying()) {
+    song.pause();
+  } else {
+    song.play();
+  }
 }
