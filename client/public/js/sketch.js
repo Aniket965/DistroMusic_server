b5e5506fda05cc451befd335e76198fcbee0fd7b
aniket965 <aniket965.as@gmail.com>
2018-01-22 14:31:12 +0530

    Added Visualizer

--- client/public/js/sketch.js
+++ client/public/js/sketch.js
@@ -1,22 +1,29 @@
 let song;
-let amp;
+let fft;
 function setup() {
-
-  amp = new p5.Amplitude()
-  song = loadSound('http://localhost:3000/assets/lol.mp3', () => {
-    document.getElementById('playButton').addEventListener('click', () => togglePlaying())
-    console.log('loaded')
-  })
- let div = document.getElementById('sv')
- console.log(div.getAttribute('width'))
+  amp = new p5.Amplitude();
+  song = loadSound("http://localhost:3000/assets/lol.mp3", () => {
+    document
+      .getElementById("playButton")
+      .addEventListener("click", () => togglePlaying());
+    console.log("loaded");
+  });
+  fft = new p5.FFT();
+  let div = document.getElementById("sv");
   var canvas = createCanvas(div.offsetWidth, div.offsetHeight);
- canvas.parent('sv');
- background(255, 0, 200);
-
+  canvas.parent("sv");
+  background(15, 32, 50);
 }
 function draw() {
-    background(244, 248, 252);
-rect(mouseX, mouseY,100, 100);
+  background(15, 32, 50);
+  var spectrum = fft.analyze();
+  noStroke();
+  fill(255); // spectrum is green
+  for (var i = 0; i< spectrum.length; i= i+20){
+    var x = map(i, 0, spectrum.length, 0, width);
+    var h = -height + map(spectrum[i], 0, 255, height, 0);
+    rect(x, height, 10, h )
+  }
 }
 const togglePlaying = () => {
   if (song.isPlaying()) {
@@ -24,4 +31,4 @@ const togglePlaying = () => {
   } else {
     song.play();
   }
-}
+};
