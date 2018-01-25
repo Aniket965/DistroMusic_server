85c55d9e6d2c74b733685939ea5f86a3eff72e97
ishita agarwal <32492187+agarwalishita@users.noreply.github.com>
2018-01-25 21:57:59 +0530

    Add files via upload

--- client/public/js/sketch.js
+++ client/public/js/sketch.js
@@ -8,18 +8,20 @@ let song_name;
 let playPercent;
 function setup() {
   amp = new p5.Amplitude();
-  song = loadSound("http://localhost:3000/assets/lol.mp3", () => {
+  song = loadSound("http://localhost:3000/assets/bom_diggi.mp3", () => {
     document
       .getElementById("playButton")
       .addEventListener("click", () => togglePlaying());
     console.log("loaded");
     song.play();
+    angleMode(DEGREES)
   });
   document
     .getElementById("mute-toggle")
     .addEventListener("click", () => muteToggle());
 
   fft = new p5.FFT();
+  w=width/64;
 }
 function draw() {
   var div = document.getElementById("sv");
@@ -29,11 +31,18 @@ function draw() {
   var spectrum = fft.analyze();
   noStroke();
   fill(255); // spectrum is green
-  for (var i = 0; i < spectrum.length; i = i + 20) {
-    var x = map(i, 0, spectrum.length, 0, width);
-    var h = -height + map(spectrum[i], 0, 255, height, 0);
-    rect(x, height, 10, h);
-
+  // for (var i = 0; i < spectrum.length; i = i + 20) {
+  //   var x = map(i, 0, spectrum.length, 0, width);
+  //   var h = -height + map(spectrum[i], 0, 255, height, 0);
+  //   rect(x, height, 10, h);
+  // }
+  for(var i=0;i<spectrum.length;i+=10)
+  {
+    var amp=spectrum[i];
+    var y=map(amp,0,255,height,0)
+    rect(i*w,y,w,height-y);
+    fill(255)
+  }
     duration = song.duration();
     current_time = song.currentTime();
     left_time = current_time - duration;
@@ -41,13 +50,14 @@ function draw() {
     var d1 = new Date(null);
     var d2  = new Date(null);
     d1.setSeconds(current_time); // specify value for SECONDS here
+    console.log(d1);
     d2.setSeconds(duration);
     document.getElementById("current_time").innerHTML =  d1.toISOString().substr(11, 8);
     document.getElementById("left_time").innerHTML = d2.toISOString().substr(11,8);
-    document.getElementById("song_name").innerHTML = "lol";
+    document.getElementById("song_name").innerHTML = "bom-diggi";
 
     // console.log(playPercent)
-  }
+  
   document.getElementById('loader').style.width = `${Math.floor(playPercent)}%`
 }
 const togglePlaying = () => {
